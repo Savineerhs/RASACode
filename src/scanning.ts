8dd52b5fbce2d7c94eba6e52e6fc26af4f2f5846
Shree <shree.mandpe@gmail.com>
2023-01-14 16:39:27 +0530

    Added activity bar icon and configs for selective error checking

--- src/scanning.ts
+++ src/scanning.ts
@@ -19,34 +19,40 @@ export function scanDomainFile(contributionFile: string, domain: Domain, trainin
 	const actionsUsedInStoriesAndRules: string[] = trainingData.usedActions;
 	const intentsTrainedInNLU: string[] = trainingData.trainedIntents;
 
+	const config = vscode.workspace.getConfiguration('rasacode');
 	
 	let diagnosticsInFile: vscode.Diagnostic[] = [];
 
+
 	domain.contributions[contributionFile]["intents"].forEach(function(declaration: any) 
 	{
-		if (!intentsUsedInStoriesAndRules.includes(declaration["declaration"]))
-		{
-			const range = new vscode.Range(declaration["position"]["line"], declaration["position"]["col"], declaration["position"]["line"], declaration["position"]["col"] + declaration["length"]);
-			const diagnostic = new vscode.Diagnostic(range, "Intent " + declaration["declaration"] + " is not being used in any rule or story.", vscode.DiagnosticSeverity.Warning); 
-			diagnosticsInFile.push(diagnostic); 
-		}
 
-		if (!intentsTrainedInNLU.includes(declaration["declaration"]))
-		{
-			const range = new vscode.Range(declaration["position"]["line"], declaration["position"]["col"], declaration["position"]["line"], declaration["position"]["col"] + declaration["length"]);
-			const diagnostic = new vscode.Diagnostic(range, "Intent " + declaration["declaration"] + " has no training data associated in NLU.", vscode.DiagnosticSeverity.Warning); 
-			diagnosticsInFile.push(diagnostic); 
-		}
+		if (config.get("warnings") as any ["showWarningsForUnusedIntents"])
+			if (!intentsUsedInStoriesAndRules.includes(declaration["declaration"]))
+			{
+				const range = new vscode.Range(declaration["position"]["line"], declaration["position"]["col"], declaration["position"]["line"], declaration["position"]["col"] + declaration["length"]);
+				const diagnostic = new vscode.Diagnostic(range, "Intent " + declaration["declaration"] + " is not being used in any rule or story.", vscode.DiagnosticSeverity.Warning); 
+				diagnosticsInFile.push(diagnostic); 
+			}
+
+		if (config.get("warnings") as any ["showWarningsForUntrainedIntents"])
+			if (!intentsTrainedInNLU.includes(declaration["declaration"]))
+			{
+				const range = new vscode.Range(declaration["position"]["line"], declaration["position"]["col"], declaration["position"]["line"], declaration["position"]["col"] + declaration["length"]);
+				const diagnostic = new vscode.Diagnostic(range, "Intent " + declaration["declaration"] + " has no training data associated in NLU.", vscode.DiagnosticSeverity.Warning); 
+				diagnosticsInFile.push(diagnostic); 
+			}
 	});
 
 	domain.contributions[contributionFile]["actions"].forEach(function(declaration: any) 
 	{
-		if (!actionsUsedInStoriesAndRules.includes(declaration["declaration"]))
-		{
-			const range = new vscode.Range(declaration["position"]["line"], declaration["position"]["col"], declaration["position"]["line"], declaration["position"]["col"] + declaration["length"]);
-			const diagnostic = new vscode.Diagnostic(range, "Action " + declaration["declaration"] + " is not being used in any rule or story.", vscode.DiagnosticSeverity.Warning); 
-			diagnosticsInFile.push(diagnostic); 
-		}
+		if (config.get("warnings") as any ["showWarningsForUnusedActions"])
+			if (!actionsUsedInStoriesAndRules.includes(declaration["declaration"]))
+			{
+				const range = new vscode.Range(declaration["position"]["line"], declaration["position"]["col"], declaration["position"]["line"], declaration["position"]["col"] + declaration["length"]);
+				const diagnostic = new vscode.Diagnostic(range, "Action " + declaration["declaration"] + " is not being used in any rule or story.", vscode.DiagnosticSeverity.Warning); 
+				diagnosticsInFile.push(diagnostic); 
+			}
 	});
 
 	diagnosticCollection.set(vscode.Uri.file(contributionFile), diagnosticsInFile);
@@ -69,56 +75,63 @@ export function scanTrainingDataFile(contributionFile: string, domain: Domain, t
 	const intentsInDomain: string[] = domain.availableIntents; 
 	const actionsInDomain: string[] = domain.availableActions; 
 	
+	const config = vscode.workspace.getConfiguration('rasacode');
+
 	let diagnosticsInFile: vscode.Diagnostic[] = []; 
 
 	trainingData.contributions[contributionFile]["nlu"].forEach(function(declaration: any) 
 	{
-		if (!intentsInDomain.includes(declaration["declaration"]))
-		{
-			const range = new vscode.Range(declaration["position"]["line"], declaration["position"]["col"], declaration["position"]["line"], declaration["position"]["col"] + declaration["length"]);
-			const diagnostic = new vscode.Diagnostic(range, "Intent " + declaration["declaration"] + " has not been declared in the domain yet.", vscode.DiagnosticSeverity.Error); 
-			diagnosticsInFile.push(diagnostic); 
-		}
+		if (config.get("errors") as any ["showErrorsForUndeclaredNLU"])
+			if (!intentsInDomain.includes(declaration["declaration"]))
+			{
+				const range = new vscode.Range(declaration["position"]["line"], declaration["position"]["col"], declaration["position"]["line"], declaration["position"]["col"] + declaration["length"]);
+				const diagnostic = new vscode.Diagnostic(range, "Intent " + declaration["declaration"] + " has not been declared in the domain yet.", vscode.DiagnosticSeverity.Error); 
+				diagnosticsInFile.push(diagnostic); 
+			}
 	});
 
 	trainingData.contributions[contributionFile]["stories"]["intents"].forEach(function(declaration: any)
 	{
-		if (!intentsInDomain.includes(declaration["declaration"]))
-		{
-			const range = new vscode.Range(declaration["position"]["line"], declaration["position"]["col"], declaration["position"]["line"], declaration["position"]["col"] + declaration["length"]);
-			const diagnostic = new vscode.Diagnostic(range, "Intent " + declaration["declaration"] + " has not been declared in the domain yet.", vscode.DiagnosticSeverity.Error); 
-			diagnosticsInFile.push(diagnostic);
-		}
+		if (config.get("errors") as any ["showErrorsForUndeclaredIntents"])
+			if (!intentsInDomain.includes(declaration["declaration"]))
+			{
+				const range = new vscode.Range(declaration["position"]["line"], declaration["position"]["col"], declaration["position"]["line"], declaration["position"]["col"] + declaration["length"]);
+				const diagnostic = new vscode.Diagnostic(range, "Intent " + declaration["declaration"] + " has not been declared in the domain yet.", vscode.DiagnosticSeverity.Error); 
+				diagnosticsInFile.push(diagnostic);
+			}
 	})
 
 	trainingData.contributions[contributionFile]["stories"]["actions"].forEach(function(declaration: any)
 	{
-		if (!actionsInDomain.includes(declaration["declaration"]))
-		{
-			const range = new vscode.Range(declaration["position"]["line"], declaration["position"]["col"], declaration["position"]["line"], declaration["position"]["col"] + declaration["length"]);
-			const diagnostic = new vscode.Diagnostic(range, "Action " + declaration["declaration"] + " has not been declared in the domain yet.", vscode.DiagnosticSeverity.Error); 
-			diagnosticsInFile.push(diagnostic);
-		}
+		if (config.get("errors") as any ["showErrorsForUndeclaredActions"])
+			if (!actionsInDomain.includes(declaration["declaration"]))
+			{
+				const range = new vscode.Range(declaration["position"]["line"], declaration["position"]["col"], declaration["position"]["line"], declaration["position"]["col"] + declaration["length"]);
+				const diagnostic = new vscode.Diagnostic(range, "Action " + declaration["declaration"] + " has not been declared in the domain yet.", vscode.DiagnosticSeverity.Error); 
+				diagnosticsInFile.push(diagnostic);
+			}
 	})
 
 	trainingData.contributions[contributionFile]["rules"]["intents"].forEach(function(declaration: any)
 	{
-		if (!intentsInDomain.includes(declaration["declaration"]))
-		{
-			const range = new vscode.Range(declaration["position"]["line"], declaration["position"]["col"], declaration["position"]["line"], declaration["position"]["col"] + declaration["length"]);
-			const diagnostic = new vscode.Diagnostic(range, "Intent " + declaration["declaration"] + " has not been declared in the domain yet.", vscode.DiagnosticSeverity.Error); 
-			diagnosticsInFile.push(diagnostic);
-		}
+		if (config.get("errors") as any ["showErrorsForUndeclaredIntents"])
+			if (!intentsInDomain.includes(declaration["declaration"]))
+			{
+				const range = new vscode.Range(declaration["position"]["line"], declaration["position"]["col"], declaration["position"]["line"], declaration["position"]["col"] + declaration["length"]);
+				const diagnostic = new vscode.Diagnostic(range, "Intent " + declaration["declaration"] + " has not been declared in the domain yet.", vscode.DiagnosticSeverity.Error); 
+				diagnosticsInFile.push(diagnostic);
+			}
 	})
 
 	trainingData.contributions[contributionFile]["rules"]["actions"].forEach(function(declaration: any)
 	{
-		if (!actionsInDomain.includes(declaration["declaration"]))
-		{
-			const range = new vscode.Range(declaration["position"]["line"], declaration["position"]["col"], declaration["position"]["line"], declaration["position"]["col"] + declaration["length"]);
-			const diagnostic = new vscode.Diagnostic(range, "Action " + declaration["declaration"] + " has not been declared in the domain yet.", vscode.DiagnosticSeverity.Error); 
-			diagnosticsInFile.push(diagnostic);
-		}
+		if (config.get("errors") as any ["showErrorsForUndeclaredActions"])
+			if (!actionsInDomain.includes(declaration["declaration"]))
+			{
+				const range = new vscode.Range(declaration["position"]["line"], declaration["position"]["col"], declaration["position"]["line"], declaration["position"]["col"] + declaration["length"]);
+				const diagnostic = new vscode.Diagnostic(range, "Action " + declaration["declaration"] + " has not been declared in the domain yet.", vscode.DiagnosticSeverity.Error); 
+				diagnosticsInFile.push(diagnostic);
+			}
 	})
 
 	diagnosticCollection.set(vscode.Uri.file(contributionFile), diagnosticsInFile);
